8e469e480c09908e21cc7006d76dfac1e5f17920
Simon Wessel <simon.w.karlsson@gmail.com>
2015-02-10 23:38:19 +0100

    Cleaned up the interface

--- mobisession.py
+++ mobisession.py
@@ -1,10 +1,11 @@
-import sqlite3
-import uuid
-from flask import Flask, g, render_template, redirect, request, session, url_for
+import cStringIO, qrcode, sqlite3, uuid
+from flask import Flask, g, render_template, redirect, request, send_file, session, url_for
 
 app = Flask(__name__)
 
 DATABASE = 'database.db'
+SERVER_IP = '192.168.0.12:5000'
+STATIC_DIR = 'static'
 
 def get_db():
     db = getattr(g, '_database', None)
@@ -25,14 +26,14 @@ def index():
 	Read and write session data.
 	'''
 
-	if 'id' not in session:
+	if 'sid' not in session:
 		app.logger.debug("Generating new session id")
-		session['id'] = uuid.uuid4().hex
+		session['sid'] = uuid.uuid4().hex
 
 	if request.method == 'POST':
-		get_db().execute('insert or replace into sessions values (?, ?)', (session['id'], request.form['data']))
+		get_db().execute('insert or replace into sessions values (?, ?)', (session['sid'], request.form['data']))
 		get_db().commit()
-	data = get_db().execute('select session_data from sessions where session_id=?', (session['id'],)).fetchone()
+	data = get_db().execute('select session_data from sessions where session_id=?', (session['sid'],)).fetchone()
 	if data is not None:
 		session['data'] = data[0]
 	else:
@@ -40,21 +41,36 @@ def index():
 
 	return render_template('mobisession.html', **locals())
 
-@app.route('/<session_id>')
-def get_session(session_id=None):
+@app.route('/session/<session_id>')
+def get_session(session_id):
 	'''
 	Sets the session id and redirects to index.
 	'''
-	if session_id is not None:
-		app.logger.debug("Using stored session id")
-		session['id'] = session_id
+	app.logger.debug("Using stored session id")
+	session['sid'] = session_id
 
 	return redirect(url_for('index'))
+
+@app.route('/qr.png')
+def get_qr():
+	message = "".join(("http://", SERVER_IP, '/session/', session['sid']))
+	app.logger.debug("Message: " + message)
+	img = qrcode.make(message)
+	img_buf = cStringIO.StringIO()
+	img.save(img_buf)
+	img_buf.seek(0)
+	return send_file(img_buf, mimetype='image/png')
+
+@app.route('/static/<path:filename>')
+def get_static(filename):
+	return send_from_directory(STATIC_DIR, filename)
 
 app.secret_key = 'Change me'
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	# Deleting the host parameter will only make the app reachable from
+	# the local host.
+	app.run(host='0.0.0.0', debug=True)
 
 def init_db():
 	with app.app_context():
